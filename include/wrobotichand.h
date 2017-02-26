c7b07395e7cb85e82e336224c7b76869619c8923
Milos Zivlak <zivlakmilos@gmail.com>
2017-02-26 22:49:31 +0100

    Reciving and save state in  member

--- include/wrobotichand.h
+++ include/wrobotichand.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <thread>
+#include <mutex>
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
 
@@ -12,8 +13,28 @@ public:
     WRoboticHand(QWidget *parent = nullptr);
     ~WRoboticHand(void);
 
-    void openCommunication(const QString &uri);
-    void closeCommunication(void);
+    enum Mode {
+        ModeAutomatic = 0,
+        ModeManual,
+        ModeLock
+    };
+
+    struct State
+    {
+        Mode mode                   = ModeAutomatic;
+        bool constructionDown       = false;
+        bool constructionUp         = false;
+        bool left                   = false;
+        bool right                  = false;
+        bool rotationDown           = false;
+        bool rotationUp             = false;
+        bool extendsUnextended      = false;
+        bool extendsExtended        = false;
+        bool picked                 = false;
+    };
+
+    void openConnection(const QString &url);
+    void openConnection(void);
 
 protected:
     void paintEvent(QPaintEvent *event) override;
@@ -21,9 +42,17 @@ protected:
 private:
     typedef websocketpp::client<websocketpp::config::asio_client> WSClient;
 
-    WSClient m_socket;
+    void messageRecived(const websocketpp::connection_hdl &hdl, const WSClient::message_ptr &msg);
 
-	std::thread m_reciverThread;
+    WSClient m_socket;
+    std::thread m_reciverThread;
+
+    std::mutex m_mutexState;
+    State m_state;
 };
+
+bool operator==(const WRoboticHand::State &lhs, const WRoboticHand::State &rhs);
+inline bool operator!=(const WRoboticHand::State &lhs, const WRoboticHand::State &rhs)
+    { return !operator==(lhs, rhs); };
 
 #endif // _W_ROBOTIC_HAND_H_
--- src/wrobotichand.cpp
+++ src/wrobotichand.cpp
@@ -1,6 +1,7 @@
 #include  "wrobotichand.h"
 
 #include <QtGui>
+#include "json.hpp"
 
 #include "exception.h"
 
@@ -8,7 +9,7 @@ WRoboticHand::WRoboticHand(QWidget *parent)
     : QWidget(parent)
 {
     try {
-        openCommunication("ws://localhost:8272/");
+        openConnection("ws://localhost:8272/");
     } catch(Exception &ex) {
         qDebug() << ex;
     }
@@ -17,42 +18,73 @@ WRoboticHand::WRoboticHand(QWidget *parent)
 WRoboticHand::~WRoboticHand(void)
 {
     try {
-        closeCommunication();
+        openConnection();
     } catch(Exception &ex) {}
 }
 
-void WRoboticHand::openCommunication(const QString &uri)
+void WRoboticHand::openConnection(const QString &url)
 {
     try {
-		m_socket.set_message_handler([this](const websocketpp::connection_hdl &hdl,
-											const WSClient::message_ptr &msg)
-			{
-				qDebug() << "Radi!";
-			});
+        m_socket.set_message_handler([this](const websocketpp::connection_hdl &hdl,
+                                            const WSClient::message_ptr &msg)
+            {
+                messageRecived(hdl, msg);
+            });
         m_socket.init_asio();
 
         websocketpp::lib::error_code error;
-        WSClient::connection_ptr con = m_socket.get_connection(uri.toStdString(), error);
+        WSClient::connection_ptr con = m_socket.get_connection(url.toStdString(), error);
         if(error)
             throw Exception("Communication error: " + std::string(error.message()));
 
         m_socket.connect(con);
-		m_reciverThread = std::thread([this]()
-			{
-				m_socket.run();
-			});
+        m_reciverThread = std::thread([this]()
+            {
+                m_socket.run();
+            });
     } catch(websocketpp::exception &ex) {
         throw Exception("Communication error: " + std::string(ex.what()));
     }
 }
 
-void WRoboticHand::closeCommunication(void)
+void WRoboticHand::openConnection(void)
 {
     try {
-		if(m_reciverThread.joinable())
+        if(m_reciverThread.joinable())
 			m_reciverThread.join();
     } catch(websocketpp::exception &ex) {
         throw Exception("Communication error: " + std::string(ex.what()));
+    }
+}
+
+void WRoboticHand::messageRecived(const websocketpp::connection_hdl &hdl, const WSClient::message_ptr &msg)
+{
+    std::string message = msg->get_payload();
+    nlohmann::json data = nlohmann::json::parse(message);
+
+    State state;
+    state.constructionDown = data["construction_down"];
+    state.constructionUp = data["construction_up"];
+    state.left= data["construction_left"];
+    state.right = data["construction_right"];
+    state.rotationDown = data["rotation_down"];
+    state.rotationUp = data["rotation_up"];
+    state.extendsUnextended = data["extends_unextended"];
+    state.extendsExtended = data["extends_extended"];
+    state.picked = data["picked"];
+
+    std::string mode = data["mode"].get<std::string>();
+    if(mode == "automatic")
+        state.mode = ModeAutomatic;
+    else if(mode == "manual")
+        state.mode = ModeManual;
+    else
+        state.mode = ModeLock;
+
+    if(m_state != state)
+    {
+        m_state = state;
+        repaint();
     }
 }
 
@@ -65,4 +97,27 @@ void WRoboticHand::paintEvent(QPaintEvent *event)
      */
     p.setBrush(Qt::green);
     p.drawRect(10, height() / 2, width() * 2 / 3, 25);
+
+    /*
+     * Draw rotation
+     */
+    p.setBrush(Qt::red);
+    p.drawEllipse(50, 50, 10, 10);
+}
+
+bool operator==(const WRoboticHand::State &lhs, const WRoboticHand::State &rhs)
+{
+    if(lhs.mode != rhs.mode ||
+        lhs.constructionDown != rhs.constructionDown ||
+        lhs.constructionUp != rhs.constructionUp ||
+        lhs.left != rhs.left ||
+        lhs.right != rhs.right ||
+        lhs.rotationDown != rhs.rotationDown ||
+        lhs.rotationUp != rhs.rotationUp ||
+        lhs.extendsUnextended != rhs.extendsUnextended ||
+        lhs.extendsExtended != rhs.extendsExtended ||
+        lhs.picked != rhs.picked)
+        return false;
+
+    return true;
 }
